05c48ac9a1968382fb4fdd01b07d044e036056f1
ebamberger1 <ethansb00@gmail.com>
2022-02-16 17:44:26 -0600

    Month transition of calendar and documentation

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,18 +1,34 @@
+// scheduler.js
 var calendar = {
 	months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	/* month is 0-11 */
 	month: new Date().getMonth(),
+	/* four digit year */
 	year: new Date().getFullYear(),
-	populate: function(){
-		document.getElementById("calendarmonth").innerHTML = this.months[this.month] + " " + this.year;
-		let firstDateOfMonth = new Date(this.year, this.month, 0);
-		let firstDayOfMonth = firstDateOfMonth.getDay();
-		let tableVals = document.getElementsByTagName("td");
-		let numDaysInMonth = new Date(this.year, this.month+1, 0).getDate();
-		let i=firstDayOfMonth;
-		let ctr;
-		for (ctr = 1; ctr<numDaysInMonth+1; ctr++){
-			tableVals[firstDayOfMonth + ctr].innerHTML = ctr;
-		}
+	/* Returns the current month of the calendar*/
+	getMonth: function(){return this.month;},
+	/* Returns the current year of the calendar*/
+	getYear: function(){return this.month;},
+	/* Populates the calendar with dates */
+	initialize: function(){
+		populateCalendar(this.months[this.month], this.month, this.year);
 	},
+	/* Increments month. If month is 11, month becomes 0 and year is incrmented. */
+	incrementMonth: function(){
+		this.year = this.year + Math.trunc((this.month + 1)/12)
+		this.month = (this.month + 1) % 12;
+		this.initialize();
+	},
+	/* Decrements month. If month is 0, month becomes 11 and year is decremented. */
+	decrementMonth: function(){
+		if(this.month === 0){
+			this.month = 11;
+			this.year -= 1;
+		}
+		else{
+			this.month -= 1;
+		}
+		this.initialize();
+	}
 }
-calendar.populate();
+calendar.initialize();
---
+++ assets/js/scheduler_view.js
@@ -0,0 +1,31 @@
+// scheduler_view.js
+
+function populateCalendar(month_txt, month, year){
+	// Label month and year of calendar displayed.
+	document.getElementById("calendarmonth").innerHTML = month_txt + " " + year;
+	let firstDayOfMonth = new Date(year, month, 1).getDay(); // Day of week of first day of month (Sun(0) - Sat(6))
+	let tableVals = document.getElementsByTagName("td");
+	let numDaysInMonth = new Date(year, month+1, 0).getDate(); // Number of days of the current month
+	let ctr;
+	// Put empty strings in table values prior to first day.
+	for(ctr = 0; ctr<firstDayOfMonth; ctr++){
+		tableVals[ctr].innerHTML = "";
+	}
+	// Populate correct positions of table with dates.
+	let i=1;
+	for(ctr = firstDayOfMonth; ctr<firstDayOfMonth + numDaysInMonth; ctr++){
+		tableVals[ctr].innerHTML = i;
+		i++;
+	}
+	// Put empty strings in table values after end of month.
+	for(ctr = firstDayOfMonth + numDaysInMonth; ctr<tableVals.length; ctr++){
+		tableVals[ctr].innerHTML = "";
+	}
+	// If the last row of the table is empty, hide it.
+	if(tableVals[35].innerHTML == ""){
+		tableVals[35].parentElement.style.visibility = "hidden";
+	}
+	else{
+		tableVals[35].parentElement.style.visibility = "visible";
+	}
+}

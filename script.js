1700e5cc98fc2cf0938791331b95abf88dc2298e
tsharris14 <70899874+tsharris14@users.noreply.github.com>
2022-02-21 16:18:39 -0600

    form validation testing w/o server

---
+++ script.js
@@ -0,0 +1,21 @@
+const uname = document.getElementById('username')
+const password = document.getElementById('password')
+const form = document.getElementById('submitForm')
+const errorElement = document.getElementById('error')
+
+//check for errors before allowing user to submit
+form.addEventListener('submit', (e) => {
+    let messages = []
+    //if any forms are empty
+    if (uname.value === ''|| uname.value == null){
+        messages.push('Username is required')
+    }
+    if (password.value === ''|| password.value == null){
+        messages.push('Password is required')
+    }
+    //seperate error messages
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
+})

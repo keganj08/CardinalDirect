1cb9cdf29c0d8b406cdb45cb7b84c55d95ee0eed
TaCoya S. Harris <tsharris@noctrl.edu>
2022-03-15 22:52:36 -0500

    Add/delete notes complete for now

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -5,37 +5,36 @@ const addNoteButton = notesContainer.querySelector(".add-note");
 getNotes().forEach(note => {
     const noteElement = createNoteElement(note.id, note.content);
     notesContainer.insertBefore(noteElement, addNoteButton);
-})
-
+});
 
 
 addNoteButton.addEventListener("click", () => addNote());
     
 
 function getNotes(){
-    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+    return JSON.parse(localStorage.getItem("CardinalDirect-notes") || "[]");
 }
 
 function saveNotes(notes){
-    localStorage.setItem("stickynotes-notes" . JSON.stringify(notes));
+    localStorage.setItem("CardinalDirect-notes" , JSON.stringify(notes));
 }
 
 function createNoteElement(id, content){
     const element = document.createElement("textarea");
-    element.classlist.add("note");
+    element.classList.add("note");
     element.value = content;
     element.placeholder = "Empty note";
 
     element.addEventListener("change", () =>{
         updateNote(id, element.value);
-    })
+    });
 
     element.addEventListener("dblclick", () => {
-        const doDelete = confirm("Are you sure you want to delete?");
+        const doDelete = confirm("Are you sure you want to delete this note?");
         if (doDelete){
             deleteNote(id, element);
         }
-    })
+    });
     return element;
 }
 
@@ -54,12 +53,13 @@ function addNote(){
 
 function updateNote(id, newContent){
     const notes = getNotes();
-    const target = notes.filter(note => note.id == id)[0];
+    const targetNote = notes.filter(note => note.id == id)[0];
     targetNote.content = newContent;
     saveNotes(notes);
 }
 
 function deleteNote(id, element){
-    console.log("Deleting Note...");
-    console.log(id, newContent);
+    const notes = getNotes().filter(note => note.id != id);
+    saveNotes(notes);
+    notesContainer.removeChild(element);
 }

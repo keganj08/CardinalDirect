865caaef41d80d8302a331a4cadaff403aad27b3
tsharris <tsharris@noctrl.edu>
2022-03-24 23:50:06 -0500

    username (allow uppercase) and phone number validation

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var routes = require('./routes.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { connect } = require('mongodb');
+const { connection } = require('mongoose');
 const app = express();
 const port = 3000;
 //app.use(bodyParser.urlencoded({extended:false}));
@@ -10,6 +12,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/', routes);
 
-//app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, ()=> console.log("Server running on port: " + port));
+
+
+
+
+

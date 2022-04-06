653bb7823cde5da012f9faa4f64bf8afc4a36aa4
tsharris <tsharris@noctrl.edu>
2022-04-06 13:03:42 -0500

    node password reset working - add css

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const { reset } = require('nodemon');
 var mydb = require('./dbmgr.js');
+
 
 router.post('/new_user', function(req, res){
 	let username = req.body.username;
@@ -358,5 +361,141 @@ router.post('/settings', function(req, res){
 		});
 }
 });
+
+//FORGOT PASSWORD
+router.get('/forgot-password', (req, res, next) => {
+    res.render('forgot-password');
+})
+
+const JWT_SECRET = 'secret'
+router.post('/forgot-password', (req, res, next) => {
+    const { email } = req.body;
+
+	mydb.findUserRecByEmail(email, function(result){
+		user = {
+			email: result[0].email,
+			username: result[0].username,
+			pwd: result[0].pwd,
+			pnum : result[0].pnum
+		  
+		}
+
+		if (email !== user.email){
+			res.send("user not found");
+			return;
+		}
+
+		//reset link
+		const secret = JWT_SECRET + user.pwd;
+		const payload = {
+			email: user.email,
+			username: user.username
+		};
+		const token = jwt.sign(payload, secret, {expiresIn: '1hr'});
+		
+		const link = `http://localhost:3000/reset-password/${user.email}/${token}`;
+		console.log(link);
+
+		//send email
+		let transporter = nodemailer.createTransport({
+			service: "Gmail",
+			auth:{
+				user: "directcardinal@gmail.com",
+				pass: "csindustries"
+			}
+		});
+
+		let mailOptions = {
+			from: "CardinalDirect",
+			to: user.email,
+			subject: "Password Reset Link",
+			text: link
+		};
+
+		transporter.sendMail(mailOptions, function(err, success){
+			if(err){
+				console.log(err)
+			}else{
+				console.log("Email Sent")
+				res.send('Password reset link has been sent!')
+			}
+		});
+	});
+
+})
+
+router.get('/reset-password/:email/:token', (req, res, next) => {
+    const { email, token } = req.params;
+	console.log(email);
+
+	mydb.findUserRecByEmail(email, function(result){
+		user = {
+			email: result[0].email,
+			username: result[0].username,
+			pwd: result[0].pwd,
+			pnum : result[0].pnum	
+		}
+		console.log(user.email);//invalid signature
+		//check if user is in database - later
+		if(email !== user.email){
+			res.send(user.email)
+			return;
+		}
+		const secret = JWT_SECRET + user.pwd;
+		try {
+			const payload = jwt.verify(token,secret)
+			res.render('reset-password', {email: user.email})
+		} catch (error) {
+			console.log(error.message);
+			res.send(error.message);
+		}
+	});
+})
+
+router.post ('/reset-password/:email/:token', (req, res, next) => {
+    const { email, token } = req.params;
+    const {pwd, pwd2} = req.body;
+	mydb.findUserRecByEmail(email, function(result){
+		user = {
+			email: result[0].email,
+			username: result[0].username,
+			pwd: result[0].pwd,
+			pnum : result[0].pnum
+		}
+
+		//check if username in db
+		if(email !== user.email){
+			res.send("USER NOT FOUND");
+			return;
+		}
+		
+		const secret = JWT_SECRET + user.pwd;
+		try {
+			const payload = jwt.verify(token, secret);
+
+			//check for matching passwords - later
+
+			console.log("Update Password");
+			let newRec = {
+				"username" : user.username,
+				"pwd" : req.body.pwd,
+				"pnum" : user.pnum,
+				"email" : user.email
+			};
+			mydb.updateAccount(newRec, function(result){
+				res.send(result);
+			});
+
+			//hash password - later
+			user.pwd = pwd;
+			res.send(user);
+
+		} catch (error) {
+			console.log(error.message);
+			res.send(error.message);	
+		}
+	});
+})
+
 
 module.exports = router;

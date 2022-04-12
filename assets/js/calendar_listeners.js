026d3a4b4d61ecde85b2d337e32665848c5e32fa
ebamberger1 <ethansb00@gmail.com>
2022-04-11 20:00:36 -0500

    Fix today and select features of calendar

--- assets/js/calendar_listeners.js
+++ assets/js/calendar_listeners.js
@@ -1,22 +1,63 @@
 // calendar_listeners.js
 
+function getUserEmail(){
+	let url = window.location.href;
+	let idx = url.indexOf("?user=");
+	let email = "";
+	if(idx !== -1){
+		email = url.substr(idx + 6) + "@noctrl.edu";
+	}
+	return email;
+}
+
 /* Next button on calendar increments the month */
 document.querySelector("#right_cal_button").addEventListener('click', e => {
+	//Clear the today marking if changing the month
+	let todayElem = document.getElementById("today");
+	if(todayElem !== null){
+		todayElem.id = "";
+	}
+	//Show the next month
 	calendar.incrementMonth();
 });
 
 /* Prev button on calendar decrements the month */
 document.querySelector("#left_cal_button").addEventListener('click', e => {
+	//Clear the today marking if changing the month
+	let todayElem = document.getElementById("today");
+	if(todayElem !== null){
+		todayElem.id = "";
+	}
+	//Show the previous month
 	calendar.decrementMonth();
 });
 
+// Click on a day of the calendar
+document.querySelector("#calendar tbody").addEventListener('click', e=>{
+	const tableElement = e.target;
+	if((tableElement !== null) && tableElement.tagName.toLowerCase() === "td"){
+		//add clicked date to top of event div
+		document.getElementById('eventDate').innerHTML = tableElement.innerHTML + " " + document.getElementById("calendarmonth").innerHTML;
+		
+		//Remove "selected" from class list of previous selection and add to new target's class list
+		let prevSelects = document.getElementsByClassName("selected");
+		if(prevSelects.length > 0){ //There should only be one element in prevSelects at most
+			prevSelects[0].classList.remove("selected");
+		}
+		tableElement.classList.add("selected");
+	}
+});
+
 //"Logout" button
-document.getElementById("logout").addEventListener('click', e => {
+document.getElementById("logout").addEventListener('click', function(){window.location.href = 'login.html';});
+
+// Click on Back Button
+document.getElementById("back-button").addEventListener('click', e => {
 	let url = window.location.href;
 	let idx = url.indexOf("?user=");
 	let user = "";
 	if(idx !== -1){
 		user = url.substr(idx);
 	}
-	window.location.href = 'login.html';
+	window.location.href = 'scheduler_landing.html' + user;
 });
--- assets/js/calendar_view.js
+++ assets/js/calendar_view.js
@@ -3,16 +3,12 @@
 function populateCalendar(month_txt, month, year){
 	// Label month and year of calendar displayed.
 	document.getElementById("calendarmonth").innerHTML = month_txt + " " + year;
-	document.getElementById("monthDay").innerHTML = " " + month_txt + " " + year;
-
 
 	let firstDayOfMonth = new Date(year, month, 1).getDay(); // Day of week of first day of month (Sun(0) - Sat(6))
 	let tableVals = document.getElementsByTagName("td");
 	let numDaysInMonth = new Date(year, month+1, 0).getDate(); // Number of days of the current month
 	let ctr;
 	
-	
-	console.log(today.getDate());
 	// Put empty strings in table values prior to first day.
 	for(ctr = 0; ctr<firstDayOfMonth; ctr++){
 		tableVals[ctr].innerHTML = "";
@@ -22,19 +18,12 @@ function populateCalendar(month_txt, month, year){
 	for(ctr = firstDayOfMonth; ctr<firstDayOfMonth + numDaysInMonth; ctr++){
 		tableVals[ctr].innerHTML = i;
 		i++;
-
-		//mark today's date
-		if ((today.getDate()+1).toString() === ctr.toString()){
-			tableVals[ctr].className = "today";
-		}
 	}
-
 	// Put empty strings in table values after end of month.
 	for(ctr = firstDayOfMonth + numDaysInMonth; ctr<tableVals.length; ctr++){
 		tableVals[ctr].innerHTML = "";
-
-		
 	}
+	
 	// If the last row of the table is empty, hide it.
 	if(tableVals[35].innerHTML == ""){
 		tableVals[35].parentElement.style.visibility = "hidden";
@@ -42,30 +31,25 @@ function populateCalendar(month_txt, month, year){
 	else{
 		tableVals[35].parentElement.style.visibility = "visible";
 	}
+	
+	//Remove "selected" from class list of previous selection
+	let prevSelects = document.getElementsByClassName("selected");
+	if(prevSelects.length > 0){ //There should only be one element in prevSelects at most
+		prevSelects[0].classList.remove("selected");
+	}
+	
+	let today = new Date(); // Current day object
+	// If the month displayed is the current month, highlight the current day
+	if(today.getMonth() === month){
+		//mark today's date in the calendar
+		tableVals[firstDayOfMonth + today.getDate() - 1].id = "today";
+		document.getElementById('eventDate').innerHTML = today.getDate() + " " + month_txt + " " + year;
+	}
+	else{
+		// Add "selected" to class list of first day of month
+		tableVals[firstDayOfMonth].classList.add("selected");
+		document.getElementById('eventDate').innerHTML = 1 + " " + month_txt + " " + year;
+	}
 
-	document.getElementById('eventDate').innerHTML = today.getDate().toString() + " " + document.getElementById("monthDay").innerHTML;
-
+	
 }
-
-let today = new Date();
-document.querySelectorAll("td").forEach(day => {
-	day.addEventListener("click", event => {
-		console.log(event.currentTarget.innerHTML);//day clicked
-		//add clicked date to top of event div
-	
-		document.getElementById("eventDate").innerHTML = " ";
-		document.getElementById('eventDate').innerHTML = event.currentTarget.innerHTML + document.getElementById("monthDay").innerHTML;
-
-		event.currentTarget.classList.toggle("selected");
-
-		})
-});
-/* Prev button on calendar decrements the month */
-document.querySelector("#left_cal_button").addEventListener('click', e => {
-	calendar.decrementMonth();
-	document.getElementById("selected").innerHTML = " ";
-});
-	
-
-
-

b3ba55f264299fd373032619ca144515fae17e8e
tsharris <tsharris@noctrl.edu>
2022-04-25 15:08:09 -0500

    createRoute "enter" key functional

--- assets/js/interior.js
+++ assets/js/interior.js
@@ -36,6 +36,9 @@ document.addEventListener('DOMContentLoaded', e => {
 		});		
 	}
 });
+document.getElementById("roomForm").addEventListener('submit', e => {
+  e.preventDefault();
+});
 document.getElementById("backButton").addEventListener('click', e => {
 	let url = window.location.href;
 	let idx = url.indexOf("?user=");
